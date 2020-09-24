62564013ad051bd731b3abb3ab0fb37e4d9be6ca
Hussain Fatmi <hadifatmi@gmail.com>
2020-09-24 10:46:08 -0400

    implemented main activity

--- HelloToast2/app/src/main/java/com/example/hellotoast2/MainActivity.java
+++ HelloToast2/app/src/main/java/com/example/hellotoast2/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.hellotoast2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void sayHello(View view){
+
+    }
+
+    public void countUp(View view){
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
