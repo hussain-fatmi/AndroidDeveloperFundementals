13efdfa977f00d93d3c0d9d1ce7e397652a62c10
Hussain Fatmi <hadifatmi@gmail.com>
2020-09-24 10:29:40 -0400

    intial commit

--- ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +10,9 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static int openSpace = 1;
+    public static int ADD = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void launchSecondActivity(View view){
-        Log.d(LOG_TAG, "Add Item Clicked");
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Add Item Clicked");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityforResult(intent, ADD);
     }
 }
--- ShoppingList/app/src/main/java/com/example/shoppinglist/SecondActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/SecondActivity.java
@@ -3,6 +3,9 @@ package com.example.shoppinglist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void itemAdd(View view){
+        Button b = (Button)view;
+        Log.d("LOG", b.getText().toString());
+
     }
 }
