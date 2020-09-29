d9e92c64fef74dd0032c284fe356b40d01c79a11
Hussain Fatmi <hadifatmi@gmail.com>
2020-09-29 13:06:52 -0400

    Implemented counter

--- CounterText/app/src/main/java/com/example/countertext/MainActivity.java
+++ CounterText/app/src/main/java/com/example/countertext/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.countertext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    public void countUp(View view){
+        mCount++;
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
