c11c830dd8079a164c83d9904e44d859df13683e
Hussain Fatmi <hadifatmi@gmail.com>
2020-09-29 13:17:32 -0400

    Implemented onSaveInstanceState() tosate the surrent stat of the app

--- CounterText/app/src/main/java/com/example/countertext/MainActivity.java
+++ CounterText/app/src/main/java/com/example/countertext/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.countertext;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            mCount = savedInstanceState.getInt("Counter");
+        }
         mShowCount = findViewById(R.id.show_count);
+        mShowCount.setText(Integer.toString(mCount));
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("Counter", mCount);
     }
 
     public void countUp(View view){
