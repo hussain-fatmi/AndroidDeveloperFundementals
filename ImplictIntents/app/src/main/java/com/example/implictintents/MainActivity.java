f765fbf2dd8581c55d50582121d440b5e013ec7f
Hussain Fatmi <hadifatmi@gmail.com>
2021-01-14 11:08:06 -0500

    Implemented OpenWebsite button and switched to linear layout

--- ImplictIntents/app/src/main/java/com/example/implictintents/MainActivity.java
+++ ImplictIntents/app/src/main/java/com/example/implictintents/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.implictintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openWebsite(View view) {
+        // Get the URL text.
+        mWebsiteEditText = findViewById(R.id.editText_website);
+
+        // Parse the URI and create the intent.
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Find an activity to hand the intent and start that activity.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+
     }
 }
